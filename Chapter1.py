98cef321d073a4a2b60abc3c033665017415745d
Raja <rajainfield@gmail.com>
2026-02-18 17:56:54 +0530

    No print is needed

--- Chapter1.py
+++ Chapter1.py
@@ -54,8 +54,7 @@ a, B, c = "Orange", "Banana", "Cherry"
 print(a);print(B);print(c)
 D = E = F = 'Rajarao Korlapu'
 print(D);print(E);print(F)
-print("Raja")
-print("Ravi")
+
 #Variables do not need to be declared with any particular type, and can even change type after they have been set.
 
 # Operators
